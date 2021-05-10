d3ea79449f47023297505b443905a77fff2d6289
Imran Irshad <imran.ib@live.com>
2021-05-10 04:24:56 +0300

    config

--- src/auth/jwt-strategy.ts
+++ src/auth/jwt-strategy.ts
@@ -5,7 +5,9 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtPayload } from './jwt-payload.interface';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import * as config from 'config';
 
+const jwtConfig = config.get('jwt');
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
@@ -14,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: `SomethingReallySecure`,
+      secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const ServerConfig = config.get('server');
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = process.env.PORT || ServerConfig.port;
+  await app.listen(port);
+  logger.log(`Apllication is Listening on port ${port} `);
 }
 bootstrap();
